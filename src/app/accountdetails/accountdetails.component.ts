2c71a2b3b0492cb9a1f9f30e46305e33fe1984d8
Naveenkumar <naveen.1999tamil@gmail.com>
2024-02-09 19:39:48 +0530

    change the mock data added the category part.

--- src/app/accountdetails/accountdetails.component.ts
+++ src/app/accountdetails/accountdetails.component.ts
@@ -14,7 +14,7 @@ export class AccountdetailsComponent implements OnInit {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             datasets: [
                 {
-                    label: 'First Dataset',
+                    label: 'Expense',
                     data: [1000,2000,786,3000],
                     datalabels: {
                         align: 'end',
@@ -22,21 +22,25 @@ export class AccountdetailsComponent implements OnInit {
                     },
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 205, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(201, 203, 207, 0.2)'
                       ],
                       borderColor: [
                         'rgb(255, 99, 132)',
-                        'rgb(255, 159, 64)',
-                        'rgb(255, 205, 86)',
+            
+                      ],
+                      borderWidth: 1
+                },
+                {
+                    label: 'Savings',
+                    data: [500,200,700,1000],
+                    datalabels: {
+                        align: 'end',
+                        anchor: 'start'
+                    },
+                    backgroundColor: [
+                        'rgba(75, 192, 192, 0.2)',
+                      ],
+                      borderColor: [
                         'rgb(75, 192, 192)',
-                        'rgb(54, 162, 235)',
-                        'rgb(153, 102, 255)',
-                        'rgb(201, 203, 207)'
                       ],
                       borderWidth: 1
                 },
--- src/app/expensesplitup/expensesplitup.component.ts
+++ src/app/expensesplitup/expensesplitup.component.ts
@@ -1,38 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { ColDef, ColGroupDef, GridOptions } from 'ag-grid-community';
 
 @Component({
-  selector: 'app-expensesplitup',
-  templateUrl: './expensesplitup.component.html',
-  styleUrl: './expensesplitup.component.scss'
+    selector: 'app-expensesplitup',
+    templateUrl: './expensesplitup.component.html',
+    styleUrl: './expensesplitup.component.scss'
 })
-export class ExpensesplitupComponent implements OnInit{
-  data: any;
-  options: any;
+export class ExpensesplitupComponent implements OnInit {
+    data: any;
+    options: any;
+    columnDefs: (ColDef<any, any> | ColGroupDef<any>)[] | null | undefined;
+    public themeClass: string =
+        "ag-theme-quartz";
+    public defaultColdef: ColDef = {
+        filter: true,
+        sortable: true,
+        minWidth: 90,
+        // maxWidth:150
+    }
+    gridOptions: GridOptions = {
+        rowSelection: 'single',
+        defaultColDef: this.defaultColdef,
+    }
+    rowData: any;
 
-  ngOnInit() {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue('--text-color');
+    ngOnInit() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        this.columnDefs = [
+            { field: 'trasactionNumer', headerName: 'Transaction No' },
+            { field: 'upiId', headerName: 'UPI Id' },
+            { field: 'bankName', headerName: 'Bank' },
+            { field: 'paymentPlatform', headerName: 'Payment Platform' },
+            { field: 'paidAmount', headerName: 'Paid Amount' },
+            { field: 'status', headerName: 'Status' },
+        ];
+        this.data = {
+            labels: ['Food', 'Dress', 'Rent', 'Movie', 'Outing', 'Grocery'],
+            datasets: [
+                {
+                    data: [540, 325, 702, 100, 400, 600],
+                    backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--pink-200'), documentStyle.getPropertyValue('--green-100'), documentStyle.getPropertyValue('--yellow-500'),
+                    documentStyle.getPropertyValue('--green-500'), documentStyle.getPropertyValue('--red-300')],
+                    hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--pink-100'), documentStyle.getPropertyValue('--green-100'),
+                    documentStyle.getPropertyValue('--yellow-100'),
+                    documentStyle.getPropertyValue('--green-100'), documentStyle.getPropertyValue('--red-100')]
+                }
+            ]
+        };
 
-    this.data = {
-        labels: ['A', 'B', 'C'],
-        datasets: [
-            {
-                data: [540, 325, 702],
-                backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
-                hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
-            }
-        ]
-    };
-
-    this.options = {
-        plugins: {
-            legend: {
-                labels: {
-                    usePointStyle: true,
-                    color: textColor
+        this.options = {
+            plugins: {
+                legend: {
+                    labels: {
+                        usePointStyle: true,
+                        color: textColor
+                    }
                 }
             }
-        }
-    };
-}
+        };
+    }
 }
